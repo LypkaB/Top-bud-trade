a9cd62a1855c3bff0b6eaab8bc1fc2cde586bc9b
LypkaB <lpk.bdn@gmail.com>
2023-10-16 14:20:09 +0300

    Recall modal window - create block

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+    /*<----- Presentation slider ----->*/
     const swiper = new Swiper('.banner__swiper', {
         slidesPerView: 1,
         navigation: {
@@ -9,4 +10,48 @@ window.addEventListener('DOMContentLoaded', () => {
             delay: 2500,
         },
     });
+
+    /*<----- Recall modal ----->*/
+    const fieldsRecallModal = document.querySelectorAll('.recall__form input, .recall__form textarea');
+
+    function handleFocus(event) {
+        const label = event.target.previousElementSibling;
+
+        if (label) label.classList.add('field-up');
+    }
+
+    function handleBlur(event) {
+        const field = event.target;
+        const label = field.previousElementSibling;
+
+        if (field.value.trim() === '' && label) label.classList.remove('field-up');
+    }
+
+    fieldsRecallModal.forEach(field => {
+        field.addEventListener('focus', handleFocus);
+        field.addEventListener('blur', handleBlur);
+    });
+
+    const mainHtmlTag = document.documentElement;
+    const recallBtn = document.querySelector('.header__recall');
+    const recallModal = document.querySelector('.recall__modal');
+    const recallCloseModal = document.querySelector('.recall__modal-close');
+
+    function openModal() {
+        mainHtmlTag.classList.add('overflow-hide');
+        recallModal.classList.remove('hide');
+    }
+
+    function closeModal() {
+        mainHtmlTag.classList.remove('overflow-hide');
+        recallModal.classList.add('hide');
+    }
+
+    function handleKeydown(event) {
+        if (event.keyCode === 27) closeModal();
+    }
+
+    recallBtn.addEventListener('click', openModal);
+    recallCloseModal.addEventListener('click', closeModal);
+    document.addEventListener('keydown', handleKeydown);
 })
